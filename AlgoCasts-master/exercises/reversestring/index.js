5f5a06ded848a37fd96645fe558cf59cc3d12f41
Kristopher <kristopher.williams2@gmail.com>
2020-08-27 15:57:12 -0500

    finished first challenge

---
+++ AlgoCasts-master/exercises/reversestring/index.js
@@ -0,0 +1,41 @@
+// --- Directions
+// Given a string, return a new string with the reversed
+// order of characters
+// --- Examples
+//   reverse('apple') === 'leppa'
+//   reverse('hello') === 'olleh'
+//   reverse('Greetings!') === '!sgniteerG'
+
+function reverse(str) {
+  return str.split('').reduce((rev, char) => char + rev, "")
+
+}
+
+module.exports = reverse;
+
+/* 
+MY SOLUTION 1
+function reverse(str) {
+	return str.split("").reverse().join("");
+} */
+
+/* 
+MY SOLUTION 2
+function reverse(str) {
+	let reversedStr = [];
+	for (let char in str) {
+		reversedStr.unshift(str[char]);
+	}
+	return reversedStr.join("");
+} */
+
+/* 
+Solution 3
+function reverse(str) {
+	let reversedStr = "";
+	for (let char of str) {
+    reversedStr = char + reversedStr
+	}
+	return reversedStr;
+}
+ */
