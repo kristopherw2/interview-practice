6b1ca85038a197cd53601954892c7605dfa81a70
Kristopher Williams <kristopher.williams2@gmail.com>
2020-10-31 14:31:35 -0500

    finished reverseInt question

--- AlgoCasts-master/exercises/reverseint/index.js
+++ AlgoCasts-master/exercises/reverseint/index.js
@@ -8,6 +8,19 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+  let sign = Math.sign(n)
+  let absVal = Math.abs(n)
+  let numArr = absVal.toString().split('').reverse()
+
+  if (numArr.findIndex((item) => item > "0") === -1) {
+    return parseInt(numArr.join(''))
+  } else {
+  let splicedArr = numArr.splice(numArr.findIndex(item => item > '0')).join('')
+  console.log(splicedArr)
+
+    return (parseInt(splicedArr) * sign)
+  }
+}
 
 module.exports = reverseInt;
