e34a563549abe31b336313e91c015353afc7caaf
Kristopher Williams <kristopher.williams2@gmail.com>
2020-11-13 16:47:52 -0600

    finished Anagram questions

--- AlgoCasts-master/exercises/anagrams/index.js
+++ AlgoCasts-master/exercises/anagrams/index.js
@@ -8,6 +8,67 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+const aCharMap = buildCharMap(stringA);
+const bCharMap = buildCharMap(stringB);
+
+  if(Object.keys(aCharMap).length !== Object.keys(bCharMap).length) {
+    return false;
+  }
+
+  for(let char in aCharMap) {
+    if(aCharMap[char] !== bCharMap[char]) {
+      return false;
+    }
+  }
+
+  return true;4758
+
+}
+
+function buildCharMap(str) {
+  const charMap = {};
+
+  for(let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+    charMap[char] = charMap[char] + 1 || 1;
+  }
+
+  return charMap
+}
 
 module.exports = anagrams;
+
+/* function anagrams(stringA, stringB) {
+    let changedStringA = stringA.replace(/[^\w]/g, "");
+    let changedStringB = stringB.replace(/[^\w]/g, "");
+    const charMapA = {};
+    const charMapB = {};
+
+    for (let char of changedStringA) {
+        if (!charMapA[char]) {
+            charMapA[char] = 1;
+        } else {
+            charMapA[char]++;
+        }
+    }
+
+    for (let char of changedStringB) {
+        if (!charMapB[char]) {
+            charMapB[char] = 1;
+        } else {
+            charMapB[char]++;
+        }
+    }
+
+    if (Object.keys(charMapA).length !== Object.keys(charMapB).length) {
+        return false;
+    }
+
+    for (let chars in charMapA) {
+        console.log(charMapB[chars]);
+        if (charMapA[chars] !== charMapB[chars]) {
+            return false;
+        }
+    }
+    return true;
+} */
--- AlgoCasts-master/exercises/chunk/index.js
+++ AlgoCasts-master/exercises/chunk/index.js
@@ -24,7 +24,7 @@ function chunk(array, size) {
 /* function chunk(array, size) {
     const chunked = [];
 
-    for (let element of array) {78
+    for (let element of array) {
     const last = chunked[chunked.length - 1]
 
       if(!last || last.length === size) {
