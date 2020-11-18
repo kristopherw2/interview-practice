1baf4a1ee4a8f3b65e9cb7baec13e3df9ec30d22
Kristopher Williams <kristopher.williams2@gmail.com>
2020-11-17 19:43:46 -0600

    finished steps need to finish recursion

--- AlgoCasts-master/exercises/capitalize/index.js
+++ AlgoCasts-master/exercises/capitalize/index.js
@@ -8,6 +8,22 @@
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
 function capitalize(str) {
+    let result = str[0].toUpperCase();
+
+    for (let i = 1; i < str.length; i++) {
+        if (str[i - 1] === " ") {
+            result += str[i].toUpperCase();
+        } else {
+            result += str[i];
+        }
+    }
+
+    return result;
+}
+
+module.exports = capitalize;
+
+/* function capitalize(str) {
     const capitalizedStr = str
         .split(" ")
         .map((str) => {
@@ -17,6 +33,4 @@ function capitalize(str) {
         .join(" ");
 
     return capitalizedStr;
-}
-
-module.exports = capitalize;
+} */
--- AlgoCasts-master/exercises/steps/index.js
+++ AlgoCasts-master/exercises/steps/index.js
@@ -17,6 +17,43 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, counter = 1) {
+  
+  if (counter >= n) {
+      return;
+  }
+  
+  let hash = '#'
+  let space = ' '
+
+  let stairs = hash.repeat(counter).concat(space.repeat(n - counter));
+  console.log(stairs);
+  //steps(n, counter = counter)
+}
 
 module.exports = steps;
+
+/* function steps(n) {
+    const hash = "#";
+    const space = " ";
+
+    for (let i = 1; i <= n; i++) {
+        let steps = hash.repeat().concat(space.repeat(n - i));
+        console.log(steps);
+    }
+} */
+
+/* function steps(n) {
+    for (let row = 0; row < n; row++) {
+        let stair = "";
+
+        for (let column = 0; column < n; column++) {
+            if (column <= row) {
+                stair += "#";
+            } else {
+                stair += " ";
+            }
+        }
+        console.log(stair);
+    }
+} */
